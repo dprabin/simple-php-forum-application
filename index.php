ac944130eacfdc6920f6fd3a046182bc8f2e78bb
dprabin <dprabin@yahoo.com>
2015-01-16 00:59:23 +0545

    Index file

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 //Get Template and Assign Vars
 $template = new Template('templates/frontpage.php');
 
+//Assign Variables
+$template->heading ="This is Template heading";
+
 
 //Display template
 echo $template;
