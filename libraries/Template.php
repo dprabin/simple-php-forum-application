f8e3dad74cea27dab680f94a439bce252f5d8229
dprabin <dprabin@yahoo.com>
2015-01-15 12:56:43 +0545

    Template class definition

---
+++ libraries/Template.php
@@ -0,0 +1,36 @@
+<?php
+
+//Template Class
+//Creates a Template/View object
+
+class Template {
+    //Path to template
+    protected $template;
+    //Variables passed in
+    protected $vars = array();
+    
+    //Constructor
+    public function __constructor($template){
+        $this->template = $template;
+    }
+    
+    //Get template variables
+    public function __get($key){
+        return $this->vars[$key]
+    }
+    
+    //Set template variables
+    public function __set($key, $value){
+        $this->vars[$key] = $value;
+    }
+    
+    //Convert Object to String
+    public function __toString(){
+        extract($this->vars);
+        chdir(dirname($this->template));
+        ob_start();
+        
+        include basename($this->template);
+        return ob_get_clean();
+    }
+}
