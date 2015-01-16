02c265753ba6130432b8358e79b247d5e2f6c73d
dprabin <dprabin@yahoo.com>
2015-01-16 23:56:52 +0545

    Define Template object and set variables

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php require('core/init.php'); ?>
 <?php 
+//Create Topic Object
+$topic = new Topic;
+
 //Get Template and Assign Vars
 $template = new Template('templates/frontpage.php');
 
-//Assign Variables
-$template->heading ="This is Template heading";
+//Assign Variables to template object
+$template->topics = $topic->getAllTopics();
 
 
 //Display template
