74f34e4f3ea078a3b8615a6f81b46c6aac436cc6
dprabin <dprabin@yahoo.com>
2015-01-16 16:47:49 +0545

    GetAll method added

--- libraries/Topic.php
+++ libraries/Topic.php
@@ -7,4 +7,13 @@ class Topic {
     public function __construct(){
         $this->db = new Database;
     }
+    
+    //Get All Topics
+    public function getAll(){
+        $this->db->query("select topics.*, users.username, users.avatar, categories.name from topics inner join users on topics.user_id = users.id inner join categories on topics.category_id = categories.id order by create_date desc");
+        
+        //Assign Result Set
+        $results = $this->db->resultset();
+        return $results;
+    }
 }
