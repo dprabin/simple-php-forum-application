9e8b280b43ebc5e676bad3ee8984888eb5039bbd
dprabin <dprabin@yahoo.com>
2015-01-16 17:02:04 +0545

    Made dynamif first page

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -1,55 +1,29 @@
 <?php include('includes/header.php'); ?>
-<ul id="topics">
-    <li class="topic">
-    <div class="row">
-    <div class="col-md-2">
-        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
-    </div>    
-    <div class="col-md-10">
-        <div class="topic-content pull-right">
-            <h3><a href="topic.php">How did you learn CSS and HTML?</a> </h3>
-            <div class="topic-info">
-                <a href="category.php">Development</a> >> <a href="profile.php">BradT81</a>
-            <span class="badge pull-right">3</span>
+<?php if($topics) : ?>
+    <ul id="topics">
+        <?php foreach ($topics as $topic) : ?>
+            <li class="topic">
+            <div class="row">
+            <div class="col-md-2">
+                <img class="avatar pull-left" src="templates/img/gravatar.jpg">
+            </div>    
+            <div class="col-md-10">
+                <div class="topic-content pull-right">
+                    <h3><a href="topic.php"><?php echo $topic->title; ?></a> </h3>
+                    <div class="topic-info">
+                        <a href="category.php"><?php echo $topic->title; ?></a> >> <a href="profile.php"><?php echo $topic->title; ?></a>
+                    <span class="badge pull-right">3</span>
+                    </div>
+                </div>    
             </div>
-        </div>    
-    </div>
-    </div>
-    </li>
-
-    <li class="topic">
-    <div class="row">
-    <div class="col-md-2">
-        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
-    </div>    
-    <div class="col-md-10">
-        <div class="topic-content pull-right">
-            <h3><a href="topic.php">How to change a photoshop design to a beautiful website?</a> </h3>
-            <div class="topic-info"><a href="category.php">Development</a> >> <a href="profile.php">Ramesh123</a>
-            <span class="badge pull-right">5</span>
             </div>
-        </div>    
-    </div>
-    </div>
-    </li>
+            </li>
+    <?php endforeach; ?>
+    </ul>
+<?php else : ?>
+    <p>No Topics to Display.</p>
+<?php endif; ?>
 
-    <li class="topic">
-    <div class="row">
-    <div class="col-md-2">
-        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
-    </div>
-    <div class="col-md-10">
-        <div class="topic-content pull-right">
-            <h3><a href="topic.php">Advanced mysql from triggers to stored procedures?</a> </h3>
-            <div class="topic-info"><a href="category.php">Development</a> >> <a href="profile.php">Ramesh123</a>
-            <span class="badge pull-right">5</span>
-            </div>
-        </div>    
-    </div>
-    </div>
-    </li>
-
-</ul>
 
 <h3>Forum Statistics</h3>
 <ul>
