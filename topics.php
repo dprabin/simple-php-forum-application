7e81ea5d56138b783cbdf6e4b6bbfb5c65394c47
dprabin <dprabin@yahoo.com>
2015-01-16 16:21:01 +0545

    show many topics at one place

---
+++ topics.php
@@ -0,0 +1,11 @@
+<?php require('core/init.php'); ?>
+<?php 
+//Get Template and Assign Vars
+$template = new Template('templates/topics.php');
+
+//Assign Vars
+
+//Display template
+echo $template;
+
+?>
