9e91371e32cd349508767d1b42152980d127e40e
dprabin <dprabin@yahoo.com>
2015-01-16 16:21:53 +0545

    static UI template in Bootstrap

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -1,60 +1,60 @@
 <?php include('includes/header.php'); ?>
-                        <ul id="topics">
-                            <li class="topic">
-                            <div class="row">
-                            <div class="col-md-2">
-                                <img class="avatar pull-left" src="templates/img/gravatar.jpg">
-                            </div>    
-                            <div class="col-md-10">
-                                <div class="topic-content pull-right">
-                                    <h3><a href="topic.php">How did you learn CSS and HTML?</a> </h3>
-                                    <div class="topic-info">
-                                        <a href="category.php">Development</a> >> <a href="profile.php">BradT81</a>
-                                    <span class="badge pull-right">3</span>
-                                    </div>
-                                </div>    
-                            </div>
-                            </div>
-                            </li>
-                            
-                            <li class="topic">
-                            <div class="row">
-                            <div class="col-md-2">
-                                <img class="avatar pull-left" src="templates/img/gravatar.jpg">
-                            </div>    
-                            <div class="col-md-10">
-                                <div class="topic-content pull-right">
-                                    <h3><a href="topic.php">How to change a photoshop design to a beautiful website?</a> </h3>
-                                    <div class="topic-info"><a href="category.php">Development</a> >> <a href="profile.php">Ramesh123</a>
-                                    <span class="badge pull-right">5</span>
-                                    </div>
-                                </div>    
-                            </div>
-                            </div>
-                            </li>
+<ul id="topics">
+    <li class="topic">
+    <div class="row">
+    <div class="col-md-2">
+        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
+    </div>    
+    <div class="col-md-10">
+        <div class="topic-content pull-right">
+            <h3><a href="topic.php">How did you learn CSS and HTML?</a> </h3>
+            <div class="topic-info">
+                <a href="category.php">Development</a> >> <a href="profile.php">BradT81</a>
+            <span class="badge pull-right">3</span>
+            </div>
+        </div>    
+    </div>
+    </div>
+    </li>
 
-                            <li class="topic">
-                            <div class="row">
-                            <div class="col-md-2">
-                                <img class="avatar pull-left" src="templates/img/gravatar.jpg">
-                            </div>
-                            <div class="col-md-10">
-                                <div class="topic-content pull-right">
-                                    <h3><a href="topic.php">Advanced mysql from triggers to stored procedures?</a> </h3>
-                                    <div class="topic-info"><a href="category.php">Development</a> >> <a href="profile.php">Ramesh123</a>
-                                    <span class="badge pull-right">5</span>
-                                    </div>
-                                </div>    
-                            </div>
-                            </div>
-                            </li>
-                            
-                        </ul>
-                        
-                        <h3>Forum Statistics</h3>
-                        <ul>
-                            <li>Total Number of Users: <strong>52</strong></li>
-                            <li>Total Number of Topics: <strong>10</strong></li>
-                            <li>Total Number of Categories: <strong>5</strong></li>
-                        </ul>
+    <li class="topic">
+    <div class="row">
+    <div class="col-md-2">
+        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
+    </div>    
+    <div class="col-md-10">
+        <div class="topic-content pull-right">
+            <h3><a href="topic.php">How to change a photoshop design to a beautiful website?</a> </h3>
+            <div class="topic-info"><a href="category.php">Development</a> >> <a href="profile.php">Ramesh123</a>
+            <span class="badge pull-right">5</span>
+            </div>
+        </div>    
+    </div>
+    </div>
+    </li>
+
+    <li class="topic">
+    <div class="row">
+    <div class="col-md-2">
+        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
+    </div>
+    <div class="col-md-10">
+        <div class="topic-content pull-right">
+            <h3><a href="topic.php">Advanced mysql from triggers to stored procedures?</a> </h3>
+            <div class="topic-info"><a href="category.php">Development</a> >> <a href="profile.php">Ramesh123</a>
+            <span class="badge pull-right">5</span>
+            </div>
+        </div>    
+    </div>
+    </div>
+    </li>
+
+</ul>
+
+<h3>Forum Statistics</h3>
+<ul>
+    <li>Total Number of Users: <strong>52</strong></li>
+    <li>Total Number of Topics: <strong>10</strong></li>
+    <li>Total Number of Categories: <strong>5</strong></li>
+</ul>
 <?php include('includes/footer.php'); ?>
---
+++ templates/topics.php
@@ -0,0 +1,54 @@
+<?php include('includes/header.php'); ?>
+
+<ul id="topics">
+    <li class="topic">
+    <div class="row">
+    <div class="col-md-2">
+        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
+    </div>    
+    <div class="col-md-10">
+        <div class="topic-content pull-right">
+            <h3><a href="topic.php">How did you learn CSS and HTML?</a> </h3>
+            <div class="topic-info">
+                <a href="category.php">Development</a> >> <a href="profile.php">BradT81</a>
+            <span class="badge pull-right">3</span>
+            </div>
+        </div>    
+    </div>
+    </div>
+    </li>
+
+    <li class="topic">
+    <div class="row">
+    <div class="col-md-2">
+        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
+    </div>    
+    <div class="col-md-10">
+        <div class="topic-content pull-right">
+            <h3><a href="topic.php">How to change a photoshop design to a beautiful website?</a> </h3>
+            <div class="topic-info"><a href="category.php">Development</a> >> <a href="profile.php">Ramesh123</a>
+            <span class="badge pull-right">5</span>
+            </div>
+        </div>    
+    </div>
+    </div>
+    </li>
+
+    <li class="topic">
+    <div class="row">
+    <div class="col-md-2">
+        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
+    </div>
+    <div class="col-md-10">
+        <div class="topic-content pull-right">
+            <h3><a href="topic.php">Advanced mysql from triggers to stored procedures?</a> </h3>
+            <div class="topic-info"><a href="category.php">Development</a> >> <a href="profile.php">Ramesh123</a>
+            <span class="badge pull-right">5</span>
+            </div>
+        </div>    
+    </div>
+    </div>
+    </li>
+</ul>
+
+<?php include('includes/footerr.php'); ?>
