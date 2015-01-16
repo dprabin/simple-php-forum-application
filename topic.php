0b83ff7a9a6f24b304035c63b00d733b90952b91
dprabin <dprabin@yahoo.com>
2015-01-16 16:20:26 +0545

    Show discussions in single topic

---
+++ topic.php
@@ -0,0 +1,11 @@
+<?php require('core/init.php'); ?>
+<?php 
+//Get Template and Assign Vars
+$template = new Template('templates/topic.php');
+
+//Assign Vars
+
+//Display template
+echo $template;
+
+?>
