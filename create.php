3d89360b99c63b7676ec495f5d72bb69e110933b
dprabin <dprabin@yahoo.com>
2015-01-16 16:20:00 +0545

    Create new topic

---
+++ create.php
@@ -0,0 +1,11 @@
+<?php require('core/init.php'); ?>
+<?php 
+//Get Template and Assign Vars
+$template = new Template('templates/register.php');
+
+//Assign Vars
+
+//Display template
+echo $template;
+
+?>
