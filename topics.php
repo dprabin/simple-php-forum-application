d6b44c7f9dbd99136edda22943344f352d08966d
dprabin <dprabin@yahoo.com>
2015-01-17 10:30:34 +0545

    Add variables in Template to store title and topics by category

--- topics.php
+++ topics.php
@@ -6,10 +6,20 @@ $topic = new Topic;
 //Get Template and Assign Vars
 $template = new Template('templates/frontpage.php');
 
+//Get Category id from url
+$category = isset($_GET['category']) ? $_GET['category']:null;
+
 //Assign Variables to template object
-$template->topics = $topic->getAllTopics();
+
 $template->totalTopics = $topic->getTotalTopics();
 $template->totalCategories = $topic->getTotalCategories();
+
+if (isset($category)){
+    $template->topics = $topic->getByCategory($category);
+    $template->title = 'Posts in "'.$topic->getCategory($category)['name'].'"';
+} else {
+    $template->topics = $topic->getAllTopics();
+}
 
 //Display template
 echo $template;
