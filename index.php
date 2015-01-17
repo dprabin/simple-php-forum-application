511175b99dbf19a0ab001780ab7478c4147bef39
dprabin <dprabin@yahoo.com>
2015-01-17 08:29:29 +0545

    added totalTopics and totalCategories vars to the Template class

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ $template = new Template('templates/frontpage.php');
 
 //Assign Variables to template object
 $template->topics = $topic->getAllTopics();
-
+$template->totalTopics = $topic->getTotalTopics();
+$template->totalCategories = $topic->getTotalCategories();
 
 //Display template
 echo $template;
