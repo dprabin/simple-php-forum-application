79ddbd1bbec930d7c91bc7ff736fd54157e88313
dprabin <dprabin@yahoo.com>
2015-01-17 08:30:39 +0545

    Added methods to count total categories, total topics, and replies

--- libraries/Topic.php
+++ libraries/Topic.php
@@ -16,4 +16,32 @@ class Topic {
         $results = $this->db->resultset();
         return $results;
     }
+    
+    //Get Total Number of Topics
+    public function getTotalTopics(){
+        $this->db->query('select * from topics');
+        $rows = $this->db->resultset();
+        return $this->db->rowCount();
+    }
+
+    //Get Total Number of Categories
+    public function getTotalCategories(){
+        $this->db->query('select * from categories');
+        $rows = $this->db->resultset();
+        return $this->db->rowCount();
+    }
+    
+    //Get Total Number of Replies
+    public function getTotalTopics($topic_id){
+        $this->db->query('select * from replies where topic_id = '.$topic_id);
+        $rows = $this->db->resultset();
+        return $this->db->rowCount();
+    }
+    
+    //Get Total Number of Users
+    public function getTotalUsers(){
+        $this->db->query('select * from users');
+        $rows = $this->db->resultset();
+        return $this->db->rowCount();
+    }
 }
