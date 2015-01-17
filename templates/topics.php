8c7640ae93bc85012b8fc5adbed2d464fa5ae763
dprabin <dprabin@yahoo.com>
2015-01-17 09:29:52 +0545

    Made topics more dynamic

--- templates/topics.php
+++ templates/topics.php
@@ -1,54 +1,37 @@
 <?php include('includes/header.php'); ?>
 
 <ul id="topics">
-    <li class="topic">
-    <div class="row">
-    <div class="col-md-2">
-        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
-    </div>    
-    <div class="col-md-10">
-        <div class="topic-content pull-right">
-            <h3><a href="topic.php">How did you learn CSS and HTML?</a> </h3>
-            <div class="topic-info">
-                <a href="category.php">Development</a> >> <a href="profile.php">BradT81</a>
-            <span class="badge pull-right">3</span>
-            </div>
+    <?php foreach ($topics as $topic) : ?>
+        <li class="topic">
+        <div class="row">
+        <div class="col-md-2">
+            <img class="avatar pull-left" src="templates/img/<?php echo $topic['avatar']; ?>">
         </div>    
-    </div>
-    </div>
-    </li>
+        <div class="col-md-10">
+            <div class="topic-content pull-right">
+                <h3><a href="topic.php"><?php echo $topic['title']; ?></a> </h3>
+                <div class="topic-info">
+                    <a href="topics.php?category=<?php echo urlFormat($topic['category_id']); ?>"><?php echo $topic['name']; ?></a> >> 
+                    <a href="topics.php?user=<?php echo urlFormat($topic['user_id']); ?>"><?php echo $topic['username']; ?></a> >>
+                    Posted on: <?php echo formatDate($topic['create_date']); ?>
+                <span class="badge pull-right"><?php echo replyCount($topic['id']); ?></span>
+                </div>
+            </div>    
+        </div>
+        </div>
+        </li>
+<?php endforeach; ?>
+</ul>
+<?php else : ?>
+    <p>No Topics to Display.</p>
+<?php endif; ?>
 
-    <li class="topic">
-    <div class="row">
-    <div class="col-md-2">
-        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
-    </div>    
-    <div class="col-md-10">
-        <div class="topic-content pull-right">
-            <h3><a href="topic.php">How to change a photoshop design to a beautiful website?</a> </h3>
-            <div class="topic-info"><a href="category.php">Development</a> >> <a href="profile.php">Ramesh123</a>
-            <span class="badge pull-right">5</span>
-            </div>
-        </div>    
-    </div>
-    </div>
-    </li>
 
-    <li class="topic">
-    <div class="row">
-    <div class="col-md-2">
-        <img class="avatar pull-left" src="templates/img/gravatar.jpg">
-    </div>
-    <div class="col-md-10">
-        <div class="topic-content pull-right">
-            <h3><a href="topic.php">Advanced mysql from triggers to stored procedures?</a> </h3>
-            <div class="topic-info"><a href="category.php">Development</a> >> <a href="profile.php">Ramesh123</a>
-            <span class="badge pull-right">5</span>
-            </div>
-        </div>    
-    </div>
-    </div>
-    </li>
+<h3>Forum Statistics</h3>
+<ul>
+    <li>Total Number of Users: <strong>52</strong></li>
+    <li>Total Number of Topics: <strong><?php echo $totalTopics; ?></strong></li>
+    <li>Total Number of Categories: <strong><?php echo $totalCategories; ?></strong></li>
 </ul>
 
 <?php include('includes/footerr.php'); ?>
