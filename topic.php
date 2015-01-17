56d7ab26e9600a080916cef77290129b4004dea5
dprabin <dprabin@yahoo.com>
2015-01-17 11:15:35 +0545

    Topic Controller

--- topic.php
+++ topic.php
@@ -1,9 +1,19 @@
 <?php require('core/init.php'); ?>
 <?php 
-//Get Template and Assign Vars
-$template = new Template('templates/topic.php');
+//Create Topic Object
+$topic = new Topic;
 
-//Assign Vars
+//Get Template and Assign Vars
+$template = new Template('templates/frontpage.php');
+
+//Get Category id from url
+$topic_id = $_GET['id'];
+
+//Assign Variables to template object
+
+$template->topic = $topic->getTopic($topic_id);
+$template->replies = $topic->getReplies($topic_id);
+$template->title = $topic->gettopic($topic_id)['title'];
 
 //Display template
 echo $template;
