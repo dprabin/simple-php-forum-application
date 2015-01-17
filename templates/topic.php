226fdb01be1a513812f294c60a3decd847ba843a
dprabin <dprabin@yahoo.com>
2015-01-17 11:15:54 +0545

    Topic View

--- templates/topic.php
+++ templates/topic.php
@@ -5,71 +5,17 @@
         <div class="row">
             <div class="col-md-2">
                 <div class="user-info">
-                    <img class="avatar pull-left" src="img/gravatar.jpg" />
+                    <img class="avatar pull-left" src="images/avatars/<?php echo $topic['avatar']; ?>" />
                     <ul>
-                        <li><strong>Humagain</strong></li>
-                        <li>43 posts</li>
-                        <li><a href="profile.php">Profile</a>  </li>
+                        <li><strong><?php echo $topic['username']; ?></strong></li>
+                        <li><?php echo userPostCount($topic['user_id']); ?> Posts</li>
+                        <li><a href="<?php echo BASE_URI; ?>topics.php?user=<?php echo $topic['user_id']; ?>">Profile</a>  </li>
                     </ul>
                 </div>
             </div>
         <div class="col-md-10">
             <div class="topic-content pull-right">
                 <p>I just worked in split mode in dreamweaver and paid attention to what was that. Hwo did you learn CSS and HTML? How long did it take you until you were profficient?</p>
-            </div>
-        </div>
-    </li>
-    <li class="topic topic">
-        <div class="row">
-            <div class="col-md-2">
-                <div class="user-info">
-                    <img class="avatar pull-left" src="img/gravatar.jpg" />
-                    <ul>
-                        <li><strong>Humagain</strong></li>
-                        <li>43 posts</li>
-                        <li><a href="profile.php">Profile</a>  </li>
-                    </ul>
-                </div>
-            </div>
-        <div class="col-md-10">
-            <div class="topic-content pull-right">
-                <p>Congrats on how to make a href and interesting an image ... You can learn HTML/CSS pretty fast, though how to use it in different scenarios is a whole other deal. I like to check out tutorials on how to implement newest withing HTML/CSS (HTML5/CSS3) or check out the work of others</p>
-            </div>
-        </div>
-    </li>
-    <li class="topic topic">
-        <div class="row">
-            <div class="col-md-2">
-                <div class="user-info">
-                    <img class="avatar pull-left" src="img/gravatar.jpg" />
-                    <ul>
-                        <li><strong>Humagain</strong></li>
-                        <li>43 posts</li>
-                        <li><a href="profile.php">Profile</a>  </li>
-                    </ul>
-                </div>
-            </div>
-        <div class="col-md-10">
-            <div class="topic-content pull-right">
-                <p>HTML and CSS are basic. There is not much in them to learn. Learning how basic elements interact is the most complex part including cross-browser compatibility</p>
-            </div>
-        </div>
-    </li>
-    <li class="topic topic">
-        <div class="row">
-            <div class="col-md-2">
-                <div class="user-info">
-                    <img class="avatar pull-left" src="img/gravatar.jpg" />
-                    <ul>
-                        <li><strong>Humagain</strong></li>
-                        <li>43 posts</li>
-                        <li><a href="profile.php">Profile</a>  </li>
-                    </ul>
-                </div>
-            </div>
-        <div class="col-md-10">
-            <div class="topic-content pull-right">
-                <p>Personally, I started to look at some examples and after I build some crappy sites, I learned quite well. As a recommendtaion, you can check http://www.w3schools.com. The site is pretty complete</p>
             </div>
         </div>
     </li>
