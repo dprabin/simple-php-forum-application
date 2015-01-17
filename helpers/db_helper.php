70c20a22d9dc4f465bfa344ce76d723543d4ca98
dprabin <dprabin@yahoo.com>
2015-01-17 08:51:04 +0545

    Git list of all categories in db

--- helpers/db_helper.php
+++ helpers/db_helper.php
@@ -1,13 +1,24 @@
 <?php
     //Count number of replies for each topic
-function replyCount($topic_id){
-    $db = new Database;
-    $db->query('select * from replies where topic_id = :topic_id');
-    $db->bind(':topic_id',$topic_id);
-    //Assign rows
-    $rows = $db->resultset();
-    //Get Count
-    return $db->rowCount();
-}
+    function replyCount($topic_id){
+        $db = new Database;
+        $db->query('select * from replies where topic_id = :topic_id');
+        $db->bind(':topic_id',$topic_id);
+        //Assign rows
+        $rows = $db->resultset();
+        //Get Count
+        return $db->rowCount();
+    }
+
+    //Get all categories in DB
+    function getCategories(){
+        $db=new Database;
+        $db->query('select * from categories');
+        //Run query and assign it to results variable
+        $results = $db->resultset();
+        //return result
+        return $results;
+    }
+
 
 ?>
