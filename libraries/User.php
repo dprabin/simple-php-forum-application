17567b02b28cbb706c0e2454401584e0ad291e72
dprabin <dprabin@yahoo.com>
2015-01-18 14:08:26 +0545

    debut move_upload_file

--- libraries/User.php
+++ libraries/User.php
@@ -23,7 +23,7 @@ class User {
                     if (file_exists("images/avatars/" . $_FILES['avatar']['name'])){
                         redirect('register.php', 'File already exists','error');
                     } else {
-                        move_upload_file($_FILES['avatar']['tmp_name']), "images/avatars".$_FILES['avatar']['name'];
+                        move_upload_file($_FILES['avatar']['tmp_name'], "images/avatars".$_FILES['avatar']['name']);
                         return true;
                     }
                 }
