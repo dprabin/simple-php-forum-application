b4f769be617ee9c3e8b8cdfd272f22bc3522af66
dprabin <dprabin@yahoo.com>
2015-01-18 13:59:42 +0545

    md5 encryption to passwords

--- register.php
+++ register.php
@@ -8,8 +8,8 @@ if(isset($_POST['register'])){
     $data['name'] = $_POST['name'];
     $data['email'] = $_POST['email'];
     $data['username'] = $_POST['username'];
-    $data['password'] = $_POST['password'];
-    $data['password2'] = $_POST['password2'];
+    $data['password'] = md5($_POST['password']);
+    $data['password2'] = md5($_POST['password2']);
     $data['about'] = $_POST['about'];
     $data['last_activity'] = date("Y-m-d H:i:s");
 }
