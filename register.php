ffcb0b2d3301a7b6c3a797af8ac317b7cc9cd5aa
dprabin <dprabin@yahoo.com>
2015-01-18 19:16:13 +0545

    register user form with validation

--- register.php
+++ register.php
@@ -4,6 +4,8 @@
 $topic = new Topic;
 //Create User Object
 $user = new User;
+//Create validate object
+$validate = new Validator;
 
 if(isset($_POST['register'])){
     $data = array();
@@ -15,21 +17,36 @@ if(isset($_POST['register'])){
     $data['about'] = $_POST['about'];
     $data['last_activity'] = date("Y-m-d H:i:s");
     
-    /* commented to test user validation without inserting users
-    //Upload Avatar image
-    if ($user->uploadAvatar()){
-        $data['avatar'] = $_FILES["avatar"]["name"];
+    //Required fields
+    $field_array = array('name','email','username','password','password2');
+    
+    if ($validate->isRequired($field_array)){
+        if($validate->isValidEmail($data['email'])){
+            if($validate->passwordsMatch($data['password'],$data['password2'])){
+                //Upload Avatar image
+                if ($user->uploadAvatar()){
+                    $data['avatar'] = $_FILES["avatar"]["name"];
+                } else {
+                    $data['avatar'] = 'noimage.png';
+                }
+
+                //Register User
+                if($user->register($data)){
+                    redirect('index.php', 'You are registered and can now log in','success');
+                } else {
+                    redirect('register.php', 'Something went wrong with registration','error');
+                }
+            } else {
+                redirect('register.php',"Your Passwords do not match.",'error');
+            }
+        } else {
+            redirect('register.php',"Use a valid email address.",'error');
+        }
     } else {
-        $data['avatar'] = 'noimage.png';
+        redirect('register.php',"Please fill in All required fields",'error');
     }
     
-    //Register User
-    if($user->register($data)){
-        redirect('index.php', 'You are registered and can now log in','success');
-    } else {
-        redirect('index.php', 'Something went wrong with registration','error');
-    }
-    */
+
 }
 
 //Get Template and Assign Vars
