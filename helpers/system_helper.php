88b7e7ba944c9e0199003af1cbdb7e9ea79137a7
dprabin <dprabin@yahoo.com>
2015-01-18 18:16:50 +0545

    Add function to display message if any

--- helpers/system_helper.php
+++ helpers/system_helper.php
@@ -25,4 +25,25 @@ function redirect($page=FALSE, $message=NULL, $message_type=NULL){
     exit();
 }
 
+//Display Message
+function displayMessage(){
+    if(!empty($_SESSION['message'])){
+        $message = $_SESSION['message'];
+        if(!empty($_SESSION['message_type'])){
+            $message_type = $_SESSION['message_type'];
+            //create output
+            if ($message_type == 'error'){
+                echo '<div class="alert alert-danger">'.$message.'</div>';
+            } else {
+                echo '<div class="alert alert-success">'.$message.'</div>';
+            }
+            //Unset Message
+            unset($_SESSION['message']);
+            unset($_SESSION['message_type']);
+        } else {
+            echo '';
+        }
+    }
+}
+
 ?>
