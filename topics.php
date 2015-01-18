bfe2d7958fccf8bdad873390c76ed86e2a5c5465
dprabin <dprabin@yahoo.com>
2015-01-18 10:26:44 +0545

    Changed variable name of user_id

--- topics.php
+++ topics.php
@@ -8,6 +8,8 @@ $template = new Template('templates/frontpage.php');
 
 //Get Category id from url
 $category = isset($_GET['category']) ? $_GET['category']:null;
+//Get User id from url
+$user_id = isset($_GET['user']) ? $_GET['user']:null;
 
 //Assign Variables to template object
 
@@ -17,7 +19,13 @@ $template->totalCategories = $topic->getTotalCategories();
 if (isset($category)){
     $template->topics = $topic->getByCategory($category);
     $template->title = 'Posts in "'.$topic->getCategory($category)['name'].'"';
-} else {
+} 
+if (isset($user_id)){
+    $template->topics = $topic->getByUser($user_id);
+    //$template->title = 'Posts by "'.$user->getUser($user_id)['name'].'"';
+} 
+
+if (!isset($category) && !isset($user_id)) {
     $template->topics = $topic->getAllTopics();
 }
 
