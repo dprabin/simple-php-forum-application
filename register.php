fb5eef5d719742d8bcc26483ae4201f115a7235f
dprabin <dprabin@yahoo.com>
2015-01-18 13:58:15 +0545

    add forum processing to register Controller

--- register.php
+++ register.php
@@ -3,6 +3,17 @@
 //Create Topic Object
 $topic = new Topic;
 
+if(isset($_POST['register'])){
+    $data = array();
+    $data['name'] = $_POST['name'];
+    $data['email'] = $_POST['email'];
+    $data['username'] = $_POST['username'];
+    $data['password'] = $_POST['password'];
+    $data['password2'] = $_POST['password2'];
+    $data['about'] = $_POST['about'];
+    $data['last_activity'] = date("Y-m-d H:i:s");
+}
+
 //Get Template and Assign Vars
 $template = new Template('templates/register.php');
 
