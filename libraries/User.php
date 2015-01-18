c0cc1aad268148ec5ae6903b2a0a8f8ebebf7ebe
dprabin <dprabin@yahoo.com>
2015-01-18 18:53:55 +0545

    Defined db variable and constructor to initialize db object

--- libraries/User.php
+++ libraries/User.php
@@ -1,7 +1,33 @@
 <?php
 
 class User {
+    //Initialize DB Variable
+    private $db;
     
+    //Constructor
+    public function __construct(){
+        $this->db = new Database;
+    }
+    
+    //Register User
+    public function register($data){
+        //Query
+        $this->db->query('insert into users (name, email, avatar, username, password, about, last_activity) values (:name, :email, :avatar, :username, :password, :about, :last_activity)');
+        //Bind Values
+        $this->db->bind(':name',$data['name']);
+        $this->db->bind(':email',$data['email']);
+        $this->db->bind(':avatar',$data['avatar']);
+        $this->db->bind(':username',$data['username']);
+        $this->db->bind(':password',$data['password']);
+        $this->db->bind(':about',$data['about']);
+        $this->db->bind(':last_activity',$data['last_activity']);
+        //Execute
+        if($this->db->execute()){
+            return true;
+        } else {
+            return false;
+        }
+    }
     
     //Upload User Avatar
     public function uploadAvatar(){
