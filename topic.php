200b023b71501e0e5b0ad78feb437ba543c315a2
dprabin <dprabin@yahoo.com>
2015-01-19 10:18:59 +0545

    Process reply message and post if valid

--- topic.php
+++ topic.php
@@ -6,6 +6,29 @@ $topic = new Topic;
 //Get Category id from url
 $topic_id = $_GET['id'];
 
+//Process Reply message
+if(isset($_POST['do_reply'])){
+    //Create Data Array
+    $data=array();
+    $data['topic_id'] = $topic_id;
+    $data['body'] = $_GET['body'];
+    $data['user_id'] = getUser()['user_id'];
+    
+    //Create Validator Object
+    $validate = new Validator;
+    //Required Fields
+    $field_array = array('body');
+    
+    if($validate->isRequired($field_array)){
+        if($topic->reply($data)){
+            redirect('topic.php?id='.$topic_id,'Your reply has been posted','success');
+        } else {
+            redirect('topic.php?id='.$topic_id,'Reply not posted. Something went wrong','error');
+        }
+    } else {
+        redirect('topic.php?id='.$topic_id,'Reply not posted. Message must not be empty','error');
+    }
+}
 
 //Get Template and Assign Vars
 $template = new Template('templates/topic.php');
