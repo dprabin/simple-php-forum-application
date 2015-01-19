005ea080160032be9459964b2dc12e8d28c4e2f9
dprabin <dprabin@yahoo.com>
2015-01-19 08:46:44 +0545

    Dynamically generate total user count

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <?php 
 //Create Topic Object
 $topic = new Topic;
+//Create User Object
+$user = new User;
 
 //Get Template and Assign Vars
 $template = new Template('templates/frontpage.php');
@@ -10,6 +12,7 @@ $template = new Template('templates/frontpage.php');
 $template->topics = $topic->getAllTopics();
 $template->totalTopics = $topic->getTotalTopics();
 $template->totalCategories = $topic->getTotalCategories();
+$template->totalUsers = $user->getTotalUsers();
 
 //Display template
 echo $template;
