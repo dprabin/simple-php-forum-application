efabbd32e5e9a95383b51b76c6bf0b3a4a8cc17a
dprabin <dprabin@yahoo.com>
2015-01-19 09:20:18 +0545

    added action and names to form elements

--- templates/create.php
+++ templates/create.php
@@ -1,13 +1,13 @@
 <?php include('includes/header.php'); ?>
 
-                        <form role="form">
+                        <form role="form" method="post" action="create.php">
                             <div class="form-group">
                                 <label>Topic Title</label>
                                 <input type="text" class="form-control" name="title" placeholder="Enter Post Title" />
                             </div>
                             <div class="form-group">
                                 <label>Category</label>
-                                <select class="form-control">
+                                <select class="form-control" name="category">
                                     <option>Design</option>
                                     <option>Development</option>
                                     <option>Business and Marketing</option>
@@ -20,7 +20,7 @@
                                 <textarea id="body" rows="10" cols="80" class="form-control" name="body"></textarea>
                                 <script>CKEDITOR.replace('body');</script>
                             </div>
-                            <button type="submit" class="btn btn-default">Submit</button>
+                            <button type="submit" class="btn btn-default" name="do_create">Submit</button>
                             
                         </form>
                         
